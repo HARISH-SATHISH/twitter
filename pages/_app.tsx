f4ef1b52aa794b716a7e2b0c4850dd3f0b4f6b6d
Harish580 <gowdaharish580@gmail.com>
2023-06-18 11:47:57 +0530

    backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import{GoogleOAuthProvider}from "@react-oauth/google"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <GoogleOAuthProvider clientId='836919963832-5vadb4ir0md35oi176m5af7i9uoo665g.apps.googleusercontent.com'>
+   <Component {...pageProps} />
+  </GoogleOAuthProvider>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,89 @@
-import {BsTwitter} from 'react-icons/bs'
-import{BiHome,BiUser}from 'react-icons/bi'
-import{CgHashtag}from 'react-icons/cg'
-import{IoMdNotificationsOutline}from 'react-icons/io'
-import{FiBookmark}from 'react-icons/fi'
-import{SlEnvolope}from 'react-icons/sl'
+import { BsTwitter } from 'react-icons/bs'
+import { BiHome, BiUser } from 'react-icons/bi'
+import { CgHashtag } from 'react-icons/cg'
+import { IoMdNotificationsOutline } from 'react-icons/io'
+import { FiBookmark } from 'react-icons/fi'
+import { SlEnvolope } from 'react-icons/sl'
 import { Inter } from 'next/font/google'
-import{CgMoreO}from 'react-icons/cg'
+import { CgMoreO } from 'react-icons/cg'
 import Feed from '@/components/feedsection'
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google'
+import { useCallback } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-interface Twittersidebar{
-  title:string,
-  icon:React.ReactNode
+interface Twittersidebar {
+  title: string,
+  icon: React.ReactNode
 }
 
 
-const slidebarlist: Twittersidebar[]=[
+const slidebarlist: Twittersidebar[] = [
   {
-    title:"Home",
-    icon:<BiHome/>
+    title: "Home",
+    icon: <BiHome />
   },
   {
-    title:"Explore",
-    icon:<CgHashtag/>
+    title: "Explore",
+    icon: <CgHashtag />
   },
   {
-    title:"Notification",
-    icon:<IoMdNotificationsOutline/>
+    title: "Notification",
+    icon: <IoMdNotificationsOutline />
   },
   {
-    title:"Message",
-    icon:<SlEnvolope/>
+    title: "Message",
+    icon: <SlEnvolope />
   },
   {
-    title:"Bookmark",
-    icon:<FiBookmark/>
+    title: "Bookmark",
+    icon: <FiBookmark />
   },
   {
-    title:"Profile",
-    icon:<BiUser/>
+    title: "Profile",
+    icon: <BiUser />
   },
   {
-    title:"More",
-    icon:<CgMoreO/>
+    title: "More",
+    icon: <CgMoreO />
   }
 ]
 
 export default function Home() {
+  const handleLogin =  useCallback((cred : CredentialResponse)=>{
+
+   },[])
   return (
     <div className='grid grid-cols-12 h-screen w-screen  pl-12 '>
       <div className='col-span-2'>
-        <div className='flex justify-start p-3 mt-1 text-3xl hover:bg-gray-300 w-fit h-fit rounded-full transition-all cursor-pointer'><BsTwitter/></div>
+        <div className='flex justify-start p-3 mt-1 text-3xl hover:bg-gray-300 w-fit h-fit rounded-full transition-all cursor-pointer'><BsTwitter /></div>
         <div className='mt-3 '>
           <ul >
-            {slidebarlist.map((item)=><li key={item.title} className='flex justify-start  gap-3 text-2xl items-center hover:bg-gray-300 w-fit py-1 px-3 rounded-3xl mb-2 transition-all cursor-pointer'><span>{item.icon}</span><span>{item.title}</span></li>)}
+            {slidebarlist.map((item) => <li key={item.title} className='flex justify-start  gap-3 text-2xl items-center hover:bg-gray-300 w-fit py-1 px-3 rounded-3xl mb-2 transition-all cursor-pointer'><span>{item.icon}</span><span>{item.title}</span></li>)}
           </ul>
         </div>
         <div className='px-2'>
-        <button className='text-2xl bg-black text-white w-full
+          <button className='text-2xl bg-black text-white w-full
          rounded-full  px-8 py-2 hover:bg-gray-800 transition-color mt-4 '>Tweet</button>
         </div>
       </div>
-      <div className='col-span-6  h-screen overflow-scroll pl-3'><Feed/>
-      <Feed/>
-      <Feed/>
-      <Feed/>
-      <Feed/>
-      <Feed/>
+      <div className='col-span-6  h-screen overflow-scroll pl-3'><Feed />
+        <Feed />
+        <Feed />
+        <Feed />
+        <Feed />
+        <Feed />
       </div>
-      
-      <div className='col-span-4'></div>
+
+      <div className='col-span-4'>
+        <div className=' p-2 py-3'>
+          <h1 className=' text-3xl  mb-3  font-semibold'>New to twitter?</h1>
+          <div >
+            <GoogleLogin onSuccess={(cred) => console.log(cred)} />
+          </div>
+        </div>
+      </div>
+
 
     </div>
   )
